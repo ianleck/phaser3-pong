3b64997045c97b8657e9fa53ad1651c2b3362d92
Ian <39756111+ianleck@users.noreply.github.com>
2021-10-25 01:23:04 +0800

    Add game instructions

--- src/consts/GameTexts.ts
+++ src/consts/GameTexts.ts
@@ -1,6 +1,6 @@
 // =========== Game Titles ===========
 const GameTitle = "Old School Tennis";
-const StartTitle = "Press Space to Start";
+const StartTitle = "\n \n Press Space to Start \n \n      \u2191\u2193 to play";
 const ContinueGame = "Press Space to Continue";
 
 const AIVictory = "AI Won! Better luck next time!";
